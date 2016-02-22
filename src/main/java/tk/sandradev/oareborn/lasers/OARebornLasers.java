3757eafd366945dfe9fd4fba6e75b0c3fd638ac1
Sandra <gjgfujb@gmail.com>
2016-02-23 01:05:33 +1100

    Added Crafting Recipes. Released 1.8.9-0.0.1.3

--- src/main/java/tk/sandradev/oareborn/lasers/OARebornLasers.java
+++ src/main/java/tk/sandradev/oareborn/lasers/OARebornLasers.java
@@ -2,11 +2,14 @@ package tk.sandradev.oareborn.lasers;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import tk.sandradev.oareborn.OAReborn;
 import tk.sandradev.oareborn.api.lasers.ILaserReceiverCap;
 import tk.sandradev.oareborn.internal.CommonProxy;
@@ -23,7 +26,7 @@ public class OARebornLasers {
     public static Item itemLaserProjector = null;
     public static Item itemLaserMirror = null;
     public static CommonProxy proxy = null;
-    public static SimpleNetworkWrapper network = null;
+    //public static SimpleNetworkWrapper network = null;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
@@ -44,5 +47,15 @@ public class OARebornLasers {
         proxy.registerItem(itemLaserProjector, "oareborn:itemLaserProjector").setCreativeTab(tab);
         //network = NetworkRegistry.INSTANCE.newSimpleChannel("oalasers");
         //network.registerMessage(LaserMessage.class, LaserMessage.class, 0, Side.SERVER);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        GameRegistry.addRecipe(new ShapedOreRecipe(itemLaserLens, "mgm", "s#s", "mrm", 'm', "ingotIron", 's', "ingotGold", '#', "blockGlass", 'r', "dustRedstone", 'g', "dustGlowstone"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(itemLaserMirror, "mgl", "ggg", "lgm", 'l', itemLaserLens, 'm', "ingotIron", 'g', "blockGlass"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(itemLaserProjector, "l g", " gl", "g g", 'g', "dustGlowstone", 'l', itemLaserLens));
+        GameRegistry.addRecipe(new ShapedOreRecipe(laserReceiver, "mpm", "lgm", "mgm", 'm', "ingotIron", 'p', itemLaserProjector, 'l', itemLaserLens));
+        GameRegistry.addRecipe(new ShapedOreRecipe(laserMirror, " lm", "lil", "ml ", 'm', "ingotIron", 'i', itemLaserMirror, 'l', itemLaserLens));
+        GameRegistry.addRecipe(new ShapedOreRecipe(laserPlayer, "mem", "elp", "mrm", 'm', "ingotIron", 'e', Items.ender_pearl, 'l', itemLaserLens, 'p', itemLaserProjector, 'r', "blockRedstone"));
     }
 }
